1afb8ac51bf165f731d67d25a6aaa0b25a9248a4
=Precious Kosisochukwu <=preciouskosisochukwu01@gmail.com>
2021-07-21 14:23:17 +0100

    updating

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     // database variables
-    $datasource = "mysql:host=us-cdbr-east-04.cleardb.com;dbname=marketplacedb";
+    $datasource = "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_da5ef0164b807a8";
     $username = "b0be18499afc7b";
     $password = "36837e31";
 
